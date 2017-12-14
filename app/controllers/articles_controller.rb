e941dd03a3379f8875d68936a7648efdf48d692d
Ruby on Rails <usman.ijaz@informa.com>
2017-12-14 11:14:40 +0000

    Update articles commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,41 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
-    def create
-        # render plain: params[:article].inspect
-        @article = Article.new(article_params)
-        @article.save
-        redirect_to_articles_show(@article)
+    def edit
+        @article = Article.find(params[:id])
     end
     
-    private 
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+        
+    end
+    
+    
+        
+        def update
+            @article = Article.find(params[:id])
+            if @article.update(article_params)
+                flash[:notice] = "Article was successfully updated"
+                redirect_to article_path(@article)
+            else
+                render 'edit'
+            end
+        end
+        
+        def show
+            @article = Article.find(params[:id]) 
+        end
+        
+        private 
         def article_params
            params.require(:article).permit(:title,:description) 
         end
     
 end
+ 
