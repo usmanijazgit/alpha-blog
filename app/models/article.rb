e6e71d9ea216865b76fc138b381bbf49b56d25ad
Ruby on Rails <usman.ijaz@informa.com>
2017-12-21 15:10:54 +0000

    add association between user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-    
+    belongs_to :user
     validates :title, presence:true, length: {minimum: 3, maximum: 50}
     validates :description, presence:true, length: {minimum:10, maximum: 200}
-    
+    validates :user_id, presence: true
 end
