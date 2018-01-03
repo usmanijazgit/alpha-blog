844fa742b72991ecf1fe4175106ab6377c979154
Ruby on Rails <usman.ijaz@informa.com>
2018-01-03 16:31:19 +0000

    Add Edit, Update to UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,28 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome to the alpha blog! #{@user.username}"
+            flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
         end
     end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
     
     private
     def user_params
