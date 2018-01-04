c5cf84ddfef7a9f21fd192b1df867599158a12eb
Ruby on Rails <usman.ijaz@informa.com>
2018-01-04 12:45:59 +0000

    Create show action and add Gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,12 @@ class UsersController < ApplicationController
         end
     end
     
+    def show
+       @user = User.find(params[:id]) 
+       
+       
+    end
+    
     
     private
     def user_params
